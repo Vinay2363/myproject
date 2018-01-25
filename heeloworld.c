987044f04a2fbb916f5dbfffa54766c1026b480d
Vinay2363 <kembhavi.vinay@gmail.com>
2018-01-25 03:29:23 +0000

    final commit

--- heeloworld.c
+++ heeloworld.c
@@ -1,13 +1,7 @@
 #include<stdio.h>
 int main()
 {
-<<<<<<< HEAD
-	printf("Good morning Thursday");
-=======
-	printf("Hello Clone");
-	printf("Hello wednesday");
-	Printf("Hello ");	
 
->>>>>>> 09ddb24a1fe467d2a6f1c6bc619e9b408b7f5f3b
+	printf("Good morning Thursday");
 	
 }
